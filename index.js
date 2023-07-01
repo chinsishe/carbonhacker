d10ba7dc6b30408c821b3f192d289bedf8256c46
pawelsapiecha <73373491+pawelsapiecha@users.noreply.github.com>
2023-07-01 15:41:59 -0700

    cleaned code and website, added comments

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 import '../styles/main.css';
 import * as d3 from 'd3';
-import sampleData from './sample.json';
-// index.js
-// import { initMap } from './map';
 import L from 'leaflet';
-import logoImage from './img/logo2.png';
 
 
 fetch('./sample.json')
     .then(response => response.json())
     .then(data => {
-        ////////////////////// MAP //////////////////
 
+        // MAP CONFIGURATION
         function initMap() {
             // Map setup and configuration code here
             const map = L.map('map').setView([44.967243, -103.771556], 5);
@@ -39,21 +35,11 @@ fetch('./sample.json')
             const marker2 = L.marker([40.74827905539646, -73.98639071492255], { icon: houseIcon }).addTo(map);
             const marker3 = L.marker([34.06366012952451, -118.36008635986163], { icon: houseIcon }).addTo(map);
 
-            // // Bind a popup to each marker
-            // marker1.bindPopup('Seattle Case Study');
-            // marker2.bindPopup('New York Case Study');
-            // marker3.bindPopup('Los Angeles Case Study');
-
-
         }
 
         initMap();
 
-        ///////////////////// NUMBERS ///////////////////
-
-        /////////////SLIDER///////////////////////////
-
-
+        // READING AND PROCESSING DATA
         const pvAreaSlider = document.getElementById("pv-area-slider");
 
         // Get the current value of the slider
@@ -66,7 +52,6 @@ fetch('./sample.json')
         });
 
 
-
         // Get references to the input elements and the submit button
         const zipCodeInput = document.getElementById("zip-code");
         const squareFootageInput = document.getElementById("square-footage");
@@ -75,16 +60,14 @@ fetch('./sample.json')
         const gasConsumptionEUI = document.getElementById("gas-consumption");
         const submitButton = document.getElementById("submit-button");
 
+        // default data
+        let zipCode = 85711;
+        let buildingArea = 1200;
+        let buildingType = "mid-rise-residential";
+        let electricityConsumption = 1200;
+        let gasConsumption = 150;
 
-
-
-
-        let zipCode = null;
-        let buildingArea = null;
-        let buildingType = null;
-        let electricityConsumption = null;
-        let gasConsumption = null;
-
+        // DATA UPDATE FUNCTION. RUNS WHEN "SUBMIT" IS CLICKED 
 
         function updateInput() {
             // Retrieve the values from the input elements
@@ -94,7 +77,6 @@ fetch('./sample.json')
             electricityConsumption = electricityConsumptionEUI.value;
             gasConsumption = gasConsumptionEUI.value;
 
-
             // Log the values to the console
             console.log("Zip Code:", zipCode);
             console.log("Area (SF):", buildingArea);
@@ -103,18 +85,19 @@ fetch('./sample.json')
             console.log("Gas Consumption:", gasConsumption);
         }
 
-        updateInput();
-
         // Add event listener to the submit button
         submitButton.addEventListener("click", () => {
-
             updateInput();
             updateDiagram();
-
         });
 
+        // GRAPH SVG IS BEING CREATED HERE
+
+        // Calculate the dimensions for the SVG
         const width = window.innerWidth * 0.9 - 235;
         const height = window.innerHeight * (1 / 2) - 85;
+        const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+
         // Create an SVG element with the calculated dimensions
         const svg = d3.select('#graph')
             .append('svg')
@@ -126,21 +109,13 @@ fetch('./sample.json')
         const fossilFuelNumber = d3.select("#carbon-number").append("text");
         const totalNumber = d3.select("#total-number").append("text");
 
-
+        // FUNCTION THAT RENDERS DIAGRAM WITH UPDATED DATA
         function updateDiagram(sliderValue) {
 
-            /* # EUI [kBtu/ft2/yr] from area [ft2] and electricity/gas consumption [kWh].
-            # The 3.412 factor converts between kWh and kBtu.
-            Electricity_EUI = 3.412 * Electricity_Consumption / Area
-            FossilFuel_EUI = 3.412 * FossilFuel_Consumption / Area
-            Total_EUI = Electricity_EUI + FossilFuel_EUI */
-
-            // const electricityEui = data.Electricity_EUI;
-            // const fossilFuelEui = data.FossilFuel_EUI;
-
+            ///left menu EUIs are printed here
             const electricityEui = (3.412 * electricityConsumption / buildingArea).toFixed(2);
             const fossilFuelEui = (3.412 * gasConsumption / buildingArea).toFixed(2);
-            const totalEui = +electricityEui + +fossilFuelEui;
+            const totalEui = (+electricityEui + +fossilFuelEui).toFixed(2);
             electricityNumber
                 .text(electricityEui)
                 .attr("x", 10)
@@ -159,26 +134,14 @@ fetch('./sample.json')
                 .attr("y", 20)
                 .attr("class", "eui-numbers");
 
-
-            ///////////////////// GRAPH ////////////////////
-
-            // data import
+            // data import and processing from sample file
             const Years = data.Years;
-
             const fossilFuelEmissionsUnit = (0.067 * +buildingArea * +fossilFuelEui).toFixed(2);
             const fossilFuelEmissions = Years.map(() => fossilFuelEmissionsUnit);
-            // const fossilFuelEmissionsRaW = data.FossilFuel_Emissions;
-            // const fossilFuelEmissions = fossilFuelEmissionsRaW.map((emission) => emission * 0.067 * buildingArea * fossilFuelEui);
-
             const refElectricityEui = data.RefElectricity_EUI;
             const refFossilFuelEui = data.RefFossilFuel_EUI;
             const eScale = +electricityEui / +refElectricityEui;
             const fScale = +fossilFuelEui / +refFossilFuelEui;
-
-
-            // const elecricityAverageEmissions = data.Electricity_AverageEmissions;
-
-
             const electricityAverageEmissionIntensity = data.Electricity_AverageEmissionsIntensity;
             const electricityMarginalEmissionIntensity = data.Electricity_MarginalEmissionsIntensity;
             const elecricityAverageEmissions = [];
@@ -196,9 +159,8 @@ fetch('./sample.json')
                 totalIntegratedEmissions += +yearInterval * +totalAverageEmissions[i];
             }
 
-
             // Whenever PV area percent 'PVAreaPct' is updated:
-            var pvArea = +sliderValue * +buildingArea; //////////UPDATE
+            var pvArea = +sliderValue * +buildingArea;
             console.log(pvArea);
 
             const pvAverageEmissions = [];
@@ -215,76 +177,27 @@ fetch('./sample.json')
             }
             const netIntegratedEmissions = +totalIntegratedEmissions - +pvIntegratedEmissions;
 
-            // Calculate the dimensions for the SVG
+            console.log(netAverageEmissions);
+            console.log(netMarginalEmissions);
+            console.log(pvAverageEmissions);
+
+
 
             // Set up the scales for x and y axes
             const xScale = d3.scaleLinear()
                 .domain(d3.extent(Years))
-                .range([0, width]);
+                .range([40, +width - 50]);
 
-            // const yScaleFossilFuelEmissions = d3.scaleLinear()
-            //     .domain(d3.extent(fossilFuelEmissions))
-            //     .range([height, 0]);
+            const yScaleUniversal = d3.scaleLinear()
+                .domain([-500, 500])
+                .range([+height - 50, 50]);
 
-            // const yScaleElectricityAverageEmission = d3.scaleLinear()
-            //     .domain(d3.extent(elecricityAverageEmissions))
-            //     .range([height, 0]);
+            const yScaleNetAverageEmissions = yScaleUniversal;
+            const yScaleNetMarginalEmissions = yScaleUniversal;
+            const yScaleTotalAverageEmissions = yScaleUniversal;
+            const yScaleTotalMarginalEmissions = yScaleUniversal;
 
-            // const yScaleElectricityMarginalEmissions = d3.scaleLinear()
-            //     .domain(d3.extent(electricityMarginalEmissions))
-            //     .range([height, 0]);
-
-            // const yScalePVAverageEmissions = d3.scaleLinear()
-            //     .domain(d3.extent(pvAverageEmissions))
-            //     .range([height, 0]);
-
-            // const yScalePVMarginalEmissions = d3.scaleLinear()
-            //     .domain(d3.extent(pvMarginalEmissions))
-            //     .range([height, 0]);
-
-
-            const yScaleNetAverageEmissions = d3.scaleLinear()
-                .domain(d3.extent(netAverageEmissions))
-                // .domain([-17.713300086399727, 102.89005403611276])
-                .range([height, 0]);
-
-            console.log(d3.extent(netAverageEmissions));
-
-            const yScaleNetMarginalEmissions = d3.scaleLinear()
-                .domain(d3.extent(netMarginalEmissions))
-                // .domain([-203.40285929374613, 34.98720974355429])
-                .range([height, 0]);
-
-            console.log(d3.extent(netMarginalEmissions));
-
-            // Create the line generator
-
-            // // Create the line generator
-            // const lineFossilFuelEmissions = d3.line()
-            //     .x((d, i) => xScale(Years[i]))
-            //     .y((d) => yScaleFossilFuelEmissions(d))
-            //     .curve(d3.curveMonotoneX);
-
-            // const lineElectricityAverageEmission = d3.line()
-            //     .x((d, i) => xScale(Years[i]))
-            //     .y((d) => yScaleElectricityAverageEmission(d))
-            //     .curve(d3.curveMonotoneX);
-
-            // const lineElectricityMarginalEmissions = d3.line()
-            //     .x((d, i) => xScale(Years[i]))
-            //     .y((d) => yScaleElectricityMarginalEmissions(d))
-            //     .curve(d3.curveMonotoneX);
-
-            // const linePVAverageEmissions = d3.line()
-            //     .x((d, i) => xScale(Years[i]))
-            //     .y((d) => yScalePVAverageEmissions(d))
-            //     .curve(d3.curveMonotoneX);
-
-            // const linePVMarginalEmissionsn = d3.line()
-            //     .x((d, i) => xScale(Years[i]))
-            //     .y((d) => yScalePVMarginalEmissions(d))
-            //     .curve(d3.curveMonotoneX);
-
+            //Create lines in graph
             const lineNetAverageEmissions = d3.line()
                 .x((d, i) => xScale(Years[i]))
                 .y((d) => yScaleNetAverageEmissions(d))
@@ -295,52 +208,20 @@ fetch('./sample.json')
                 .y((d) => yScaleNetMarginalEmissions(d))
                 .curve(d3.curveMonotoneX);
 
+            const lineTotalAverageEmissions = d3.line()
+                .x((d, i) => xScale(Years[i]))
+                .y((d) => yScaleTotalAverageEmissions(d))
+                .curve(d3.curveMonotoneX);
 
+            const lineTotalMarginalEmissions = d3.line()
+                .x((d, i) => xScale(Years[i]))
+                .y((d) => yScaleTotalMarginalEmissions(d))
+                .curve(d3.curveMonotoneX);
 
-
-            // // Append the line to the SVG
-            // svg.append('path')
-            //     .datum(fossilFuelEmissions)
-            //     .attr('d', lineFossilFuelEmissions)
-            //     .attr('fill', 'black')
-            //     .attr('stroke', 'blue')
-            //     .attr('stroke-width', 2);
-
-            // // Append the line to the SVG
-            // svg.append('path')
-            //     .datum(elecricityAverageEmissions)
-            //     .attr('d', lineElectricityAverageEmission)
-            //     .attr('fill', 'none')
-            //     .attr('stroke', 'red')
-            //     .attr('stroke-width', 2);
-
-            // // Append the line to the SVG
-            // svg.append('path')
-            //     .datum(electricityMarginalEmissions)
-            //     .attr('d', lineElectricityMarginalEmissions)
-            //     .attr('fill', 'none')
-            //     .attr('stroke', 'brown')
-            //     .attr('stroke-width', 2);
-
-            // // Append the line to the SVG
-            // svg.append('path')
-            //     .datum(pvAverageEmissions)
-            //     .attr('d', linePVAverageEmissions)
-            //     .attr('fill', 'none')
-            //     .attr('stroke', 'orange')
-            //     .attr('stroke-width', 2);
-
-            // // Append the line to the SVG
-            // svg.append('path')
-            //     .datum(pvMarginalEmissions)
-            //     .attr('d', linePVMarginalEmissionsn)
-            //     .attr('fill', 'none')
-            //     .attr('stroke', 'green')
-            //     .attr('stroke-width', 2);
-
-            // Append the line to the SVG
+            // This could use join, however simple solution is to remove old lines and draw new every time parameters change
             svg.selectAll('path').remove();
 
+            // Append the line to the SVG
             svg.append('path')
                 .datum(netAverageEmissions)
                 .attr('d', lineNetAverageEmissions)
@@ -354,31 +235,66 @@ fetch('./sample.json')
                 .attr('id', 'lineNetMarginalEmissions')
                 .attr('class', 'graph-lines');
 
-            ///////////////////////// AXIS LINES ////////////////////////////
+            // Append the line to the SVG
+            svg.append('path')
+                .datum(totalAverageEmissions)
+                .attr('d', lineTotalAverageEmissions)
+                .attr('id', 'lineTotalAverageEmissions')
+                .attr('class', 'graph-lines');
 
+            // Append the line to the SVG
+            svg.append('path')
+                .datum(totalMarginalEmissions)
+                .attr('d', lineTotalMarginalEmissions)
+                .attr('id', 'lineTotalMarginalEmissions')
+                .attr('class', 'graph-lines');
+
+            //Axis are created here
+            //First clean all old axis when data change
             svg.selectAll('g').remove();
 
-            // Add x-axis
-            const xAxis = d3.axisBottom(xScale);
-            svg.append('g')
-                .attr('transform', `translate(0, ${height})`)
+            //Create y axis
+            const xAxis = d3.axisBottom(xScale)
+                .tickSizeOuter(0)
+                .tickPadding(8)
+                .tickFormat(d3.format(".0f"))
+                .tickSize(6);
+
+            const xAxisGroup = svg.append('g')
+                .attr('transform', `translate(0, ${+height - 50})`)
                 .call(xAxis);
 
-            // Add y-axis
-            const yAxis = d3.axisLeft(yScaleNetAverageEmissions);
-            svg.append('g')
-                .call(yAxis);
+            // Append arrowhead marker to the x-axis
+            xAxisGroup.append('marker')
+                .attr('id', 'xAxisArrowhead')
+                .attr('markerWidth', 10)
+                .attr('markerHeight', 10)
+                .attr('refX', 9)
+                .attr('refY', 3)
+                .attr('orient', 'auto')
+                .append('path')
+                .attr('d', 'M0,0 L0,6 L9,3 z')
+                .style('fill', 'black').attr('transform', 'rotate(90)');
+
+            xAxisGroup.select('.domain')
+                .attr('marker-end', 'url(#xAxisArrowhead)');
+
+            //create y axis
+            const yAxisTotalMarginal = d3.axisLeft(yScaleTotalMarginalEmissions)
+                .tickSizeInner(-width)
+                .tickSizeOuter(0)
+                .tickPadding(8)
+                .tickFormat(d3.format(".0f"))
+                .tickSize(6);
+
+            const yAxisGroupTotalMarginal = svg.append('g')
+                .call(yAxisTotalMarginal).attr('transform', `translate(40, 0)`);
 
         }
 
         updateDiagram(sliderValue);
 
-
-
-
-
-
-        ///////////////////////////////////// ICONS //////////////////////////////
+        // Unfinished module that visualize co2 emissions. It shows how many trees you need to plant to compensate for your emissions and how many cars emit that much co2
 
         const icons = d3.select("#icons-section");
         const treeNumber = Math.floor(200 / 40);
@@ -394,8 +310,6 @@ fetch('./sample.json')
         for (let j = 0; j < carNumber; j++) {
             icons.append("img").attr("src", "https://i.imgur.com/gMhUu0I.png").attr("alt", "ThatsACar").attr("class", "car-icon");
         }
-
-
 
     })
     .catch(error => {
